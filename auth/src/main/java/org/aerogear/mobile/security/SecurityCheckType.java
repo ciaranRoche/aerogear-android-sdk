6c9f3757a727d62a554afaf5eb0c04b4046ad0a3
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-02-21 17:14:20 +0000

    debugger, emulator and screenlock checks added

--- auth/src/main/java/org/aerogear/mobile/security/SecurityCheckType.java
+++ auth/src/main/java/org/aerogear/mobile/security/SecurityCheckType.java
@@ -1,7 +1,10 @@
 package org.aerogear.mobile.security;
 
 import org.aerogear.mobile.security.checks.DeveloperModeCheck;
+import org.aerogear.mobile.security.checks.DebuggerCheck;
+import org.aerogear.mobile.security.checks.EmulatorCheck;
 import org.aerogear.mobile.security.checks.RootedCheck;
+import org.aerogear.mobile.security.checks.ScreenLockCheck;
 
 /**
  * Checks that can be performed.
@@ -18,7 +21,26 @@ public enum SecurityCheckType {
      *
      * @return <code>true</code> if developer mode is enabled in the device.
      */
-    IS_DEVELOPER_MODE(new DeveloperModeCheck());
+    IS_DEVELOPER_MODE(new DeveloperModeCheck()),
+    /**
+     * Detect if device is running in debug mode.
+     *
+     * @return <code>true</code> if debug mode is enabled in the device.
+     */
+    IS_DEBUGGER(new DebuggerCheck()),
+    /**
+     * Detect if device is an emulator
+     *
+     * @return <code>true</code> if device is an emulator.
+     */
+    IS_EMULATOR(new EmulatorCheck()),
+    /**
+     * Detect if device has a screen lock
+     *
+     * @return <code>true</code> if device has a screen lock enabled.
+     */
+    HAS_SCREEN_LOCK(new ScreenLockCheck());
+
 
     private SecurityCheck check;
 
